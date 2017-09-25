aaed168a9f3b3ff551829bb99a24101f12a51778
Erik Feeley <efeeley@vectorform.com>
2017-09-24 23:26:33 -0400

    code wars

---
+++ CodeWars/Mumbling.fs
@@ -0,0 +1,6 @@
+open System
+
+let accum (s : string) : string =
+  s.ToLower ()
+  |> Seq.mapi (fun index char -> (Char.ToUpper char |> string) + (String.replicate index (string char)))
+  |> String.concat "-"
