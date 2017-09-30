cd6f5d96eac1f4b5d9489ee858fbff2c11bf78ed
ErikFeeley <ehfeeley@gmail.com>
2017-09-30 03:11:44 -0400

    whooaa trying suave dude

---
+++ GonnaTrySuave/FirstAttempt/FirstTryGuy/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Utils.Collections
+
+let app =
+  choose
+    [ GET >=> choose
+        [ path "/" >=> OK "index wat"
+          path "/hello" >=> OK "Hello GET"
+          path "/goodbye" >=> OK "Good bye GET"]
+      POST >=> choose
+        [ path "/hello" >=> OK "Hello POST"
+          path "/goodbye" >=> OK "Good by POST"] ]
+
+[<EntryPoint>]
+let main argv =
+  startWebServer defaultConfig app
+  0
