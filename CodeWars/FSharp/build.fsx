008ae3978630cafe2d374051d02b64580fc8fc5d
Erik Feeley <efeeley@vectorform.com>
2017-10-02 15:15:04 -0400

    ok made some progress with FAEK

--- CodeWars/FSharp/build.fsx
+++ CodeWars/FSharp/build.fsx
@@ -1,6 +1,16 @@
-(* -- Fake Dependencies paket-inline
-source https://nuget.org/api/v2
-storage: none
-nuget Fake.Core.Targets prerelease
-group Main
+(* -- Fake Dependencies paket.dependencies
+file ./paket.dependencies
+group netcorebuild
 -- Fake Dependencies -- *)
+
+#load "./.fake/build.fsx/intellisense.fsx"
+#r "./packages/netcorebuild/FAKE.Core.Target/lib/netstandard2.0/Fake.Core.Target.dll"
+#r "./packages/netcorebuild/FAKE.DotNet.Cli/lib/netstandard2.0/Fake.DotNet.Cli.dll"
+
+open Fake.Core
+open Fake.DotNet.Cli
+
+Target.Create "MyBuild" (fun _ ->
+  printfn "message from mybuild target")
+
+Target.RunOrDefault "MyBuild"
