6edd113b7a6e71265afea322d10bafa957707489
Erik Feeley <efeeley@vectorform.com>
2017-10-02 11:43:33 -0400

    hmm still cant get fake working

--- CodeWars/FSharp/build.fsx
+++ CodeWars/FSharp/build.fsx
@@ -1,22 +1,6 @@
 (* -- Fake Dependencies paket-inline
 source https://nuget.org/api/v2
-
+storage: none
 nuget Fake.Core.Targets prerelease
 group Main
 -- Fake Dependencies -- *)
-
-#r @"packages/FAKE/tools/FakeLib.dll"
-
-open Fake
-
-Target "Clean" (fun _ -> trace "Cleaning stuff")
-
-Target "Build" (fun _ -> trace "Building stuff")
-
-Target "Deploy" (fun _ -> trace "Deploying stuff")
-
-"Clean"
-  ==> "Build"
-  ==> "Deploy"
-
-RunTargetOrDefault "Build"
